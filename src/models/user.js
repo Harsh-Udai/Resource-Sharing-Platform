76080777f71b4efc358212d7b9fe04849fb77ec2
HarshUdai <harshudai2001@gmail.com>
2021-02-28 14:30:26 +0530

    Some Work

---
+++ src/models/user.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        //unique: true, // all emails must be uniqe
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    tokens:[{
+        token:{
+            type: String,
+            required: true
+        }
+    }],
+    avatar:{
+        type: Buffer
+    }
+},{
+    timestamps: true
+})
+
+
+userSchema.methods.generateAuthToken = async function(){
+    const user  = this
+    //console.log(user)
+    const token = jwt.sign({_id: user._id.toString()},'resourceManagementApp')
+    //console.log(token);
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email,password)=>{
+
+    const user  = await User.findOne({email: email})
+
+    if(!user){
+        throw new Error('Unable to login /No User')
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        throw new Error ('Unable to login /password issues')
+    }
+
+    return user
+
+}
+
+// Hash the plain Text Password
+userSchema.pre('save',async function(next){
+    const user = this
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+    next()
+})
+
+
+
+
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = User;
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = new express.Router()
+const User = require('../models/user')
 
-
-router.get('/login',(req,res)=>{
-    res.send("Hello World")
+// Router for creating user at Backend
+router.post('/users/create',async (req,res)=>{
+    const user = new User(req.body)   // Data recieve from the request
+    try{
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({user,token})
+        
+    } catch(e){
+        
+        res.status(400).send(e)
+    }
 })
 
 module.exports = router
